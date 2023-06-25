589f3f1f2e97d411c6a3f03578b11adb43abd551
Ricardo Cardoso <rick.duk@gmail.com>
2023-06-25 17:56:10 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -12,5 +12,9 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string) {
+    return this.client.db().collection(name)
   }
 }
